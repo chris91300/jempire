809a59846f85f8ebdea22a6fa65f987fda2622ac
DEHAINAULT Quentin <quentin.dehainault@maif.fr>
2026-02-17 13:37:52 +0100

    feature: add variables and game loop

--- src/main.java
+++ src/main.java
@@ -1,6 +1,13 @@
 class Main{
-
+    static int bois = 0;
+    static int pierre = 0;
+    static int or = 50;
+    static int nourriture = 100;
+    static int habitants = 1;
     public static void main(String[] args){
-
+        while (habitants > 0) {
+            
+        }
+        System.out.println("Tous les habitants sont morts, Game Over !");
     }
 }
